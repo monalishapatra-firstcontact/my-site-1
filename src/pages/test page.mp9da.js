aa892933897dbd85c8110688e71a97b3dff424ab
monalishapatra-firstcontact <monalishapatra.firstcontact@gmail.com>
2025-07-06 00:28:32 +0530

    test commit

---
+++ src/pages/test page.mp9da.js
@@ -0,0 +1,6 @@
+// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+
+$w.onReady(function () {
+    console.log("Hello, World!");
+});
